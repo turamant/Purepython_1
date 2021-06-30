290eabdede632493ddb57060d1bd82b2a508c671
turamant <tur1amant@gmail.com>
2021-06-30 17:15:19 +0300

    password crypt

---
+++ src/wr_key.py
@@ -0,0 +1,9 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+if __name__=='__main__':
+    write_key()
